ca968f8a1bdbd4a04a134f1180bce4708cd0af60
mamoru2002 <tiusika.m@gmail.com>
2025-08-05 18:38:12 +0900

    ルーティングを開始完了処理のため修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-namespace :api do
-    resources :diagnosis_forms, only: [] do
+  namespace :api do
+      resources :diagnosis_results, only: [ :create ] do
+    member do
+        post :answers
+        post :complete
+      end
+    end
+
+    resources :diagnosis_forms, only: [], param: :name do
       member { get :questions }
     end
   end
