0ffc100535913b599d01bce847215a97a9097eb2
mamoru2002 <tiusika.m@gmail.com>
2025-08-05 16:12:32 +0900

    diagnosis_forms_controllerでuuidとquestion_order追加編集

--- app/controllers/api/diagnosis_forms_controller.rb
+++ app/controllers/api/diagnosis_forms_controller.rb
@@ -3,9 +3,16 @@ module Api
     def questions
       form = DiagnosisForm.find_by!(name: params[:id])
 
-      questions = form.questions.includes(:trait)
+      links = form.diagnosis_forms_questions.order(:question_order)
 
-      render json: questions.map(&:body)
+      render json: links.map { |link|
+        q = link.question
+        {
+          uuid:  q.uuid,                 # ← UUID を返す
+          body:  q.body,
+          order: link.question_order
+        }
+      }
     end
   end
 end
