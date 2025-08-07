41f6f29af727b08ea995d52c87ae2c5a6f6248ed
mamoru2002 <tiusika.m@gmail.com>
2025-08-08 00:18:48 +0900

    user_challenges_controller.rbにApi名前空間とクラス定義を追加修正

--- app/controllers/api/user_challenges_controller.rb
+++ app/controllers/api/user_challenges_controller.rb
@@ -1,18 +1,22 @@
-def create
-  ids = params.require(:challenge_ids)
+module Api
+  class UserChallengesController < ApplicationController
+    def create
+      ids = params.require(:challenge_ids)
 
-  raise "1〜4件選んでください" unless ids.length.between?(1, 4)
+      raise "1〜4件選んでください" unless ids.length.between?(1, 4)
 
-  result = current_user.diagnosis_results.find(params[:diagnosis_result_id])
+      result = current_user.diagnosis_results.find(params[:diagnosis_result_id])
 
-  ids.each do |c_id|
-    UserChallenge.create!(
-      user:            current_user,
-      challenge_id:    c_id,
-      weekly_progress: result.weekly_progress,
-      status:          :unstarted
-    )
+      ids.each do |c_id|
+        UserChallenge.create!(
+          user:            current_user,
+          challenge_id:    c_id,
+          weekly_progress: result.weekly_progress,
+          status:          :unstarted
+        )
+      end
+
+      head :created
+    end
   end
-
-  head :created
 end
