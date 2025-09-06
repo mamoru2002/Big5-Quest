b87aa933b054fb9226f778fdc4313279a5bf12b6
mamoru2002 <tiusika.m@gmail.com>
2025-09-06 19:21:32 +0900

    config/initializers/cors.rbの中身を作成

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,19 @@
-# Be sure to restart your server when you modify this file.
+require "rack/cors"
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "https://big5-quest.com", "https://www.big5-quest.com"
 
-# Read more: https://github.com/cyu/rack-cors
+    resource "/api/*",
+      headers: :any,
+      methods: %i[get post put patch delete options head],
+      credentials: true,
+      expose: %w[ETag],
+      max_age: 7200
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource "/up",
+      headers: :any,
+      methods: %i[get options head],
+      credentials: false
+  end
+end
