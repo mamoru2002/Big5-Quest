a9e64c578856cc94a07687ceeb5a51d71f0b4a58
mamoru2002 <tiusika.m@gmail.com>
2025-09-28 03:10:15 +0900

    view: DiagnosisForms#questions をJbuilder化

--- app/controllers/api/diagnosis_forms_controller.rb
+++ app/controllers/api/diagnosis_forms_controller.rb
@@ -1,20 +1,11 @@
 module Api
   class DiagnosisFormsController < ApplicationController
     def questions
-      form = DiagnosisForm.find_by!(name: params[:name])
-
-      links = form.diagnosis_forms_questions
-                  .includes(:question)
-                  .order(:question_order)
-
-      render json: links.map { |link|
-        q = link.question
-        {
-          question_uuid:  q.uuid,
-          question_body:  q.body,
-          question_order: link.question_order
-        }
-      }
+      @form  = DiagnosisForm.find_by!(name: params[:name])
+      @links = @form.diagnosis_forms_questions
+                    .includes(:question)
+                    .order(:question_order)
+      render :questions
     rescue ActiveRecord::RecordNotFound
       render json: { error: "Form not found" }, status: :not_found
     end
