2b788c358cf6cac646bf036ca7b412f1cdd06444
mamoru2002 <tiusika.m@gmail.com>
2025-10-07 15:33:21 +0900

    「プロフィール（name/bio）」の初期値を用意

--- app/controllers/api/auth/registrations_controller.rb
+++ app/controllers/api/auth/registrations_controller.rb
@@ -4,6 +4,8 @@ module Api
       def create
         ApplicationRecord.transaction do
           user = User.create!
+          UserProfile.create!(user: user, name: params[:name].presence || "ユーザー", bio: "")
+
           cred = UserCredential.new(user: user, email: params[:email])
           cred.password = params[:password]
           cred.password_confirmation = params[:password_confirmation]
@@ -11,7 +13,10 @@ module Api
 
           sign_in(:api_user_credential, cred)
           token = request.env["warden-jwt_auth.token"]
-          render json: { token:, user: { id: user.id, email: cred.email } }, status: :created
+          render json: {
+            token:,
+            user: { id: user.id, email: cred.email, name: user.user_profile.name }
+          }, status: :created
         end
       rescue ActiveRecord::RecordInvalid => e
         render json: { errors: Array(e.record&.errors&.full_messages || e.message) }, status: :unprocessable_entity
