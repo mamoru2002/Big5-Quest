92fd33b1d5d1541dc0758a08e2b362fd7142145e
mamoru2002 <tiusika.m@gmail.com>
2025-10-08 18:02:04 +0900

    ゲストログインで JWT 発行。/api の認証ルート整備し、/api/me・/api/weeks/current が 200 になるまで接続

--- config/routes.rb
+++ config/routes.rb
@@ -2,34 +2,33 @@ Rails.application.routes.draw do
   get "/up", to: proc { [ 200, { "Content-Type" => "text/plain" }, [ "ok" ] ] }
 
   namespace :api, defaults: { format: :json } do
-  devise_for :user_credentials, skip: [ :sessions, :registrations, :passwords, :confirmations ]
+    devise_for :user_credentials,
+               class_name: "UserCredential",
+               path: "",
+               skip: %i[sessions registrations passwords confirmations]
 
-  devise_scope :api_user_credential do
-    post   "login",            to: "auth/sessions#create"
-    delete "logout",           to: "auth/sessions#destroy"
-    post   "sign_up",          to: "auth/registrations#create"
-    get    "me",               to: "auth/sessions#me"
-    post   "auth/guest_login", to: "auth/guests#create"
-  end
+    devise_scope :api_user_credential do
+      post   "login",            to: "auth/sessions#create"
+      delete "logout",           to: "auth/sessions#destroy"
+      post   "sign_up",          to: "auth/registrations#create"
+      get    "me",               to: "auth/sessions#me"
+      post   "auth/guest_login", to: "auth/guests#create"
+    end
 
-
-    resources :diagnosis_results, only: [ :create, :show ] do
+    resources :diagnosis_results, only: %i[create show] do
       member do
         put  :responses
         post :complete
       end
     end
-
     resources :diagnosis_forms, only: [], param: :name do
       member { get :questions }
     end
 
-    resources :user_challenges, only: [ :index, :create, :update ]
-
+    resources :user_challenges, only: %i[index create update]
     resources :traits, only: [], param: :code do
       resources :challenges, only: [ :index ], module: :traits
     end
-
     resources :emotion_tags, only: [ :index ]
 
     get "weeks/current", to: "weeks#current"
