fc3827aae4d85c8b0865733ac040b5ba532bcbca
mamoru2002 <tiusika.m@gmail.com>
2025-10-12 04:29:20 +0900

    認証: Confirmable/Recoverable追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_10_075145) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_11_124346) do
   create_table "challenges", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "trait_id", null: false
     t.integer "difficulty", limit: 1, null: false
@@ -157,10 +157,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_10_075145) do
   create_table "user_credentials", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "email", null: false
-    t.string "password_hash", null: false
+    t.string "encrypted_password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_user_credentials_on_confirmation_token", unique: true
     t.index ["email"], name: "index_user_credentials_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_user_credentials_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_user_credentials_on_user_id", unique: true
   end
 
