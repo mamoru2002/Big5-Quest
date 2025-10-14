45fd73ccd9f8dd64f9a2bd3e1e9ed1d9c6b7c559
mamoru2002 <tiusika.m@gmail.com>
2025-10-14 10:56:34 +0900

    SES v2初期化を修正

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,16 +1,11 @@
 return unless Rails.env.production?
 
-Rails.application.configure do |config|
-  host = ENV["MAILER_HOST"] || ENV["APP_HOST"] || "api.big5-quest.com"
+host = ENV["MAILER_HOST"] || ENV["APP_HOST"] || "api.big5-quest.com"
 
-  config.action_mailer.delivery_method = :ses_v2
-  config.action_mailer.ses_v2_settings = {
-    region: ENV["AWS_REGION"] || "ap-northeast-1"
-  }
-
-  config.action_mailer.default_url_options = { host: host, protocol: "https" }
-  config.action_mailer.asset_host          = ENV["MAILER_ASSET_HOST"] || "https://#{host}"
-  config.action_mailer.default_options     = { from: ENV["MAIL_FROM"] || "no-reply@big5-quest.com" }
-  config.action_mailer.perform_caching     = false
-  config.action_mailer.raise_delivery_errors = true
-end
+ActionMailer::Base.delivery_method   = :ses_v2
+ActionMailer::Base.ses_v2_settings   = { region: ENV["AWS_REGION"] || "ap-northeast-1" }
+ActionMailer::Base.default_url_options = { host: host, protocol: "https" }
+ActionMailer::Base.asset_host        = ENV["MAILER_ASSET_HOST"] || "https://#{host}"
+ActionMailer::Base.default           from: (ENV["MAIL_FROM"] || "no-reply@big5-quest.com")
+ActionMailer::Base.perform_caching   = false
+ActionMailer::Base.raise_delivery_errors = true
