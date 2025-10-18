57e3dfc0c7b147ae8ee97c3963d0e7655178ea97
mamoru2002 <tiusika.m@gmail.com>
2025-10-19 04:08:37 +0900

    frontend(app): add /mypage route and menu link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,39 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import AppShell from './layouts/AppShell';
+import { Routes, Route, Navigate } from 'react-router-dom'
+import AppShell from './layouts/AppShell'
 
-import Welcome from './pages/Welcome';
-import DiagnosisForm from './pages/DiagnosisForm';
-import ResultPage from './pages/ResultPage';
-import ChallengeSelection from './pages/ChallengeSelection';
-import Dashboard from './pages/Dashboard';
+import Welcome from './pages/Welcome'
+import DiagnosisForm from './pages/DiagnosisForm'
+import ResultPage from './pages/ResultPage'
+import ChallengeSelection from './pages/ChallengeSelection'
+import Dashboard from './pages/Dashboard'
+import MyPage from './pages/MyPage'
 
-import SignIn from './pages/auth/SignIn';
-import SignUp from './pages/auth/SignUp';
-import VerifyNotice from './pages/auth/VerifyNotice';
-import Logout from './pages/auth/Logout';
-
-import ProtectedRoute from './components/ProtectedRoute';
+import SignIn from './pages/auth/SignIn'
+import SignUp from './pages/auth/SignUp'
+import VerifyNotice from './pages/auth/VerifyNotice'
+import Logout from './pages/auth/Logout'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
     <Routes>
       <Route element={<AppShell />}>
-        <Route index element={<Welcome />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="verify" element={<VerifyNotice />} />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/verify" element={<VerifyNotice />} />
 
         <Route element={<ProtectedRoute />}>
-          <Route path="diagnosis" element={<DiagnosisForm />} />
-          <Route path="result/:id" element={<ResultPage />} />
-          <Route path="select/:id/:code" element={<ChallengeSelection />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="/diagnosis" element={<DiagnosisForm />} />
+          <Route path="/result/:id" element={<ResultPage />} />
+          <Route path="/select/:id/:code" element={<ChallengeSelection />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/mypage" element={<MyPage />} />
         </Route>
 
-        <Route path="logout" element={<Logout />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
-
-      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  );
+  )
 }
