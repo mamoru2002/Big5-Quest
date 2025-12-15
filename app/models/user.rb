f4a381dddda1d2a3801c4c96f0015dc41e96ae30
mamoru2002 <tiusika.m@gmail.com>
2025-12-15 15:37:56 +0900

    UserProgramモデルとテーブル追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   has_many :diagnosis_results,  dependent: :destroy
   has_many :user_challenges,    dependent: :destroy
   has_many :likes,              dependent: :destroy
+  has_many :user_programs, dependent: :destroy
+
+  def active_user_program
+    user_programs.active.order(start_at: :desc).first
+  end
 end
